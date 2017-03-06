3ace45ece93c1fcfa70c1dada6948e0c6e1cbb94
valera-chemtai <valeriachemtai28@gmail.com>
2017-03-06 18:40:23 +0300

    tdd subtration, multiplication and division

--- solution.py
+++ solution.py
@@ -1,3 +1,12 @@
 def solution(num1,num2,operation):
 	if (operation=="+"):
 		return num1+num2
+	if (operation=="-"):
+		if num1>num2:
+			return num1-num2
+		else:
+			return num2-num1
+	if (operation=="*"):
+		return num1*num2
+	if (operation=="/"):
+		return num1/num2
--- test_solution.py
+++ test_solution.py
@@ -4,6 +4,9 @@ from solution import solution
 class TestSolution(unittest.TestCase):
 	def test_addition(self):
 		self.assertTrue(solution(10,20,"+"),30)
+	def test_subtraction(self):
+		self.assertTrue(solution(10,5,"-"),5)
+		
 
 
 if __name__=='__main__':
