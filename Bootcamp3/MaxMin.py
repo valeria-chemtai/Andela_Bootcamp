08c5ab90993f2382dee365021f5994d75c3d990e
valera-chemtai <valeriachemtai28@gmail.com>
2017-03-09 09:02:55 +0300

    corrected MaxMin and wordCount lab

--- Bootcamp3/MaxMin.py
+++ Bootcamp3/MaxMin.py
@@ -1,7 +1,9 @@
-def find_max_min(n):
+def min_max(n):
+    result=[]
     smallest=None
     largest=None
     firstrun=True
+
     for i in n:
         if firstrun or i<smallest:
             smallest=i
@@ -11,9 +13,9 @@ def find_max_min(n):
             largest=i
 
     if smallest==largest:
-        a=[smallest]
-
+        result.append(smallest)
     else:
-        a=[smallest,largest]
+        result.append(smallest)
+        result.append(largest)
 
-    return a
+    return result
---
+++ Bootcamp3/wordCount.py
@@ -0,0 +1,10 @@
+def word_occurance(sentence):
+	result={}
+	for word in sentence.split():
+		try: # checking for word in rest of sentence
+		    result[word]+=1
+        except: #if word does not exist in rest of word
+        	result[word]=1
+
+    for word in result:
+    	print(word,":",result[word])
