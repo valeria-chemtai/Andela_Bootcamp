ca40636fd3a71dc2d9aae87ce8430c858d2023f8
valera-chemtai <valeriachemtai28@gmail.com>
2017-03-09 13:18:10 +0300

    changes to function considering PEP8

--- Bootcamp3/wordCount.py
+++ Bootcamp3/wordCount.py
@@ -1,8 +1,11 @@
 def word_occurance(sentence):
-	result={}
+	result = {}
+	
 	for word in sentence.split():
+
 		try: # checking for word in rest of sentence
 		    result[word]+=1
+
         except: #if word does not exist in rest of word
         	result[word]=1
 
