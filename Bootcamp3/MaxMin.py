bfac316cd20ee74c0bc609f8d5e3358a9b8816eb
valera-chemtai <valeriachemtai28@gmail.com>
2017-03-09 13:13:43 +0300

    changes to function considering PEP8

--- Bootcamp3/MaxMin.py
+++ Bootcamp3/MaxMin.py
@@ -1,18 +1,18 @@
 def min_max(n):
-    result=[]
-    smallest=None
-    largest=None
-    firstrun=True
+    result = []
+    smallest = None
+    largest = None
+    firstrun = True #Initializing a True condition to aid in looping
 
     for i in n:
-        if firstrun or i<smallest:
-            smallest=i
-            firstrun=False
+        if(firstrun or i < smallest): #considering the first time of checking through the list
+            smallest = i            #After moving through the list and finding a value that is smaller, set small to be that value
+            firstrun = False     #our condition is set to false since a smaller value exists as compared to the first one
 
-        elif largest==None or i>largest:
-            largest=i
+        elif largest == None or i > largest:
+            largest = i
 
-    if smallest==largest:
+    if (smallest == largest):
         result.append(smallest)
     else:
         result.append(smallest)
